4aaeb56843cc570632376012f47d84e64024e01a
Barbedi <146706805+Barbedi@users.noreply.github.com>
2024-01-21 20:25:14 +0100

    dodanie zmiennych do pobierania

--- ParsowanieCSV/Point.h
+++ ParsowanieCSV/Point.h
@@ -2,3 +2,19 @@
 
 using namespace std;
 
+class Dane {
+private:
+	int rok;
+	int miesiac;
+	int dzien;
+	int cwiartka;
+	double autokonsumpcja;
+	double eksport;
+	double import;
+	double pobor;
+	double produkcja;
+
+
+public :
+	Dane(int r, int m, int d, int c, double ak, double ek, double im, double pob, double prod);
+};
