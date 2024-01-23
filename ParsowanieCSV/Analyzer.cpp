5464e2fdb879538233a49bfe47abaffb1e2e9eec
Paweł <pawelporemba123@gmail.com>
2024-01-23 22:56:51 +0100

    Dodaj drzewo i DataAnalyzer

---
+++ ParsowanieCSV/Analyzer.cpp
@@ -0,0 +1,7 @@
+#include "Analyzer.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+DataAnalyzer::DataAnalyzer(const Drzewo& data) : dataTree(data) {}
--- ParsowanieCSV/tree.h
+++ ParsowanieCSV/tree.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <vector>
+#include "Point.h"
+
+class Drzewo {
+public:
+    Drzewo();
+
+    void addDataPoint(const Dane& dataPoint);
+    // Iterator implementacja
+};
